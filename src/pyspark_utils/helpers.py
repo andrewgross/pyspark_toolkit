Fix HMAC calculation to work for all cases
For future readers and LLMS:

PySpark BigInt (aka a Long 64 bit int) is a signed integer representation. Previously we were
taking 8 byte words and converting them to integers. However, this meant that any hex value we
tried to convert over the signed integer limit was getting converted to NULL. In theory it should
be fine to represent it as a negative number etc since we are not doing math. However, PySpark didn't
seem to happy with that and instead converted anything larger than 2^63-1 to NULL.  This bug
robbed me of sleep for many nights.

src/pyspark_utils/helpers.py
@@ -16,7 +16,7 @@ def chars_to_int(col: StringColumn) -> LongColumn:
 
     Currently blows up if our string is too big
     """
-    return F.conv(F.hex(F.to_binary(col, F.lit("utf-8"))), 16, 10).cast("bigint")
+    return F.conv(F.hex(col), 16, 10).cast("bigint")
 
 
 def pad_key(key: ByteColumn, block_size: int) -> ByteColumn:

src/pyspark_utils/xor.py
@@ -20,6 +20,10 @@ def xor_word(
 
 def xor(col1: ByteColumn, col2: ByteColumn, byte_width: int = 64) -> ByteColumn:
 
+    # Use 4 bytes (32 bits) as the word width
+    # since we are XORing using 64 bit *signed* integers
+    # so we cant use the full width without overflow (NULL in pyspark)
+    word_width = 4
     padded_col1 = F.lpad(
         col1, byte_width, b"\x00"
     )  # Left-pad col1 with '0' up to byte_width
@@ -28,16 +32,16 @@ def xor(col1: ByteColumn, col2: ByteColumn, byte_width: int = 64) -> ByteColumn:
     )  # Left-pad col2 with '0' up to byte_width
 
     chunks = []
-    for i in range(0, byte_width, 8):
-        c1_chunk = F.substring(padded_col1, i + 1, 8)
-        c2_chunk = F.substring(padded_col2, i + 1, 8)
+    for i in range(0, byte_width, word_width):
+        c1_chunk = F.substring(padded_col1, i + 1, word_width)
+        c2_chunk = F.substring(padded_col2, i + 1, word_width)
 
         # XOR the two chunks
         xor_chunk = xor_word(c1_chunk, c2_chunk)
 
         # Convert XOR result to hexadecimal and pad it
         xor_hex_padded = F.lpad(
-            F.hex(xor_chunk), 16, "0"
+            F.hex(xor_chunk), 2 * word_width, "0"
         )  # We want string 0, not byte 0 here because it is hex
         chunks.append(xor_hex_padded)
 

tests/unit/test_helpers.py
@@ -6,9 +6,9 @@ from pyspark_utils.helpers import chars_to_int, pad_key, sha2_binary
 from tests.helpers import run_column
 
 
-def test_string_to_int():
+def test_chars_to_int():
     """
-    string_to_int should convert a string to an integer
+    chars_to_int should convert a string to an integer
     """
     a = "Hello"
     # int.from_bytes(a.encode("utf-8"), "big")
@@ -17,15 +17,35 @@ def test_string_to_int():
     assert pyspark_result == 310939249775
 
 
-def test_string_to_int_with_bytes():
+def test_chars_to_int_with_bytes():
     """
-    string_to_int should convert a bytes object to an integer
+    chars_to_int should convert a bytes object to an integer
     """
     a = b"Hello"
     # int.from_bytes(a.encode("utf-8"), "big")
     definition = chars_to_int(F.col("d1"))
     pyspark_result = run_column(definition, a, "")
     assert pyspark_result == 310939249775
+
+
+def test_chars_to_int_with_unprintable_characters():
+    """
+    chars_to_int should convert a bytes object to an integer
+    """
+    a = b"\x00\x00\x00\x00\x00\x00\x00\x00"
+    definition = chars_to_int(F.col("d1"))
+    pyspark_result = run_column(definition, a, "")
+    assert pyspark_result == 0
+
+
+def test_chars_to_int_hex_bytes_overflow():
+    """
+    chars_to_int should return None if the input is too large
+    """
+    a = b"\xFF" * 8
+    definition = chars_to_int(F.col("d1"))
+    pyspark_result = run_column(definition, a, "")
+    assert pyspark_result == None
 
 
 def test_string_to_int_types():

tests/unit/test_xor.py
@@ -5,6 +5,9 @@ from tests.helpers import run_column, xor_python
 
 
 def test_xor():
+    """
+    Test that we can XOR two strings
+    """
     a = b"Hello"
     b = b"World"
     expected_result = xor_python(a, b)
@@ -14,6 +17,9 @@ def test_xor():
 
 
 def test_xor_single():
+    """
+    Test that we can XOR two single byte strings
+    """
     a = b"a"
     b = b"b"
     definition = xor_word(F.col("d1"), F.col("d2"))
@@ -22,6 +28,9 @@ def test_xor_single():
 
 
 def test_xor_empty_byte_string():
+    """
+    Test that we can XOR two empty byte strings
+    """
     a = b"\x00" * 8
     b = b"\x36" * 8
     expected_result = xor_python(a, b)
@@ -31,6 +40,9 @@ def test_xor_empty_byte_string():
 
 
 def test_xor_bytes():
+    """
+    Test that we can XOR two byte strings
+    """
     a = b"\x00a"
     b = b"\x00b"
     definition = xor_word(F.col("d1"), F.col("d2"))
@@ -39,6 +51,9 @@ def test_xor_bytes():
 
 
 def test_xor_128():
+    """
+    Test that we can XOR two 16 byte strings
+    """
     a = b"1111111122222222"
     b = b"4444444433333333"
     expected_result = xor_python(a, b)
@@ -48,6 +63,9 @@ def test_xor_128():
 
 
 def test_xor_odd_width_inputs():
+    """
+    Test that we can XOR two strings of odd length
+    """
     a = b"111111112222222"
     b = b"444444443333333"
     byte_width = 16
@@ -58,6 +76,9 @@ def test_xor_odd_width_inputs():
 
 
 def test_xor_256():
+    """
+    Test that we can XOR two 32 byte strings
+    """
     a = b"11111111222222223333333344444444"
     b = b"44444444333333332222222211111111"
     expected_result = xor_python(a, b)
@@ -66,12 +87,14 @@ def test_xor_256():
     assert expected_result == pyspark_result
 
 
-def test_xor_hashed_key():
-    # Gotta fix this test, something is going sideways in our XOR
-    block_size = 64
-    a = b"\xd1|\xa7\x95\x17\xb1\xe2\x18\x99\xb6\xe9\xda<O/3@\xaf\xe8\xae\xc0\r\xdb\x1f\x1a#\xa9\xd6\x9e\xd6f\xed\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-    b = b"\x36" * block_size
-    expected_result = xor_python(a, b)
-    definition = xor(F.col("d1"), F.col("d2"), byte_width=block_size)
-    pyspark_result = bytes(run_column(definition, a, b)).hex()
-    assert expected_result == pyspark_result
+def test_xor_word_overflow():
+    """
+    xor_word should return None if it overflows the integer conversion
+    """
+    a = b"\xFF" * 8
+    b = b"\x36" * 8
+    definition = xor_word(F.col("d1"), F.col("d2"))
+    pyspark_result = run_column(definition, a, b)
+    # This test confirms we will overflow on the integer conversion
+    # if the value is < 2^63-1
+    assert None == pyspark_result